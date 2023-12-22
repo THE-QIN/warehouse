e37badf1b020bf08a2a4d1a1ed4b10d6e7aa469f
“THE <ainulyaqin150203@gmail.com>
2023-12-22 03:52:17 -1200

    Pesan perubahan yang telah kamu buat

--- warehouse/tests/test_input_warehouse_models.py
+++ warehouse/tests/test_input_warehouse_models.py
@@ -36,7 +36,26 @@ def test_input_warehouse():
 
 @pytest.mark.django_db
 def test_output_warehouse():
-    """
-    Buat hal yang sama, tetapi ini barang keluar
-    """
-    pass
+    # membuat record di database
+    company_indofood = Company.objects.create(name="indofood")
+    mie_kuah = Product.objects.create(name="mie kuah", company=company_indofood)
+
+    gudang_besar_selatan = Warehouse.objects.create(name="Gudang selatan")
+    gudang_besar_selatan.insert_stock(mie_kuah, 12)
+
+    gudang_selatan_stock = Stock.objects.get(warehouse=gudang_besar_selatan, products=mie_kuah)
+    assert gudang_selatan_stock.quantity == 12
+
+    # kita coba mengurangi stock barang
+    gudang_besar_selatan.remove_stock(mie_kuah, 5)
+    gudang_selatan_stock.refresh_from_db()
+    assert gudang_selatan_stock.quantity == 7
+
+    # kita coba mengurangi stock barang lagi hingga kosong
+    gudang_besar_selatan.remove_stock(mie_kuah, 7)
+    gudang_selatan_stock.refresh_from_db()
+    assert gudang_selatan_stock.quantity == 0
+
+    """ Pada kode di atas, kita melakukan hal yang sama dengan test sebelumnya,
+    tetapi dengan operasi yang berbeda. Kita menggunakan method "remove_stock" untuk mengurangi jumlah barang pada gudang.
+    Kemudian, kita test hasil jumlah barang yang telah dikurangi untuk memastikan operasi ini berjalan dengan benar. """
